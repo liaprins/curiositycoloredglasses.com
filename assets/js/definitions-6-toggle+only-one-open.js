f7b35e4083165af31c0cda09c586c52106bd57c7
Lia Prins <liajprins@gmail.com>
2017-07-03 13:09:32 -0500

    Vocab JS: added close button functionality

--- assets/js/definitions-6-toggle+only-one-open.js
+++ assets/js/definitions-6-toggle+only-one-open.js
@@ -1,8 +1,6 @@
 //collect all inline vocab words (found across post text)
 var inlineVocabWordList = document.getElementsByClassName('vocabwordinline');
 
-
-
 function vocabResize() {
 	if (window.innerWidth >= 1225) {
 		// looping through to remove the href attribute ...
@@ -26,6 +24,9 @@ window.addEventListener('resize', vocabResize, false);
 function vocabTest(e) {
 
 	if (window.innerWidth >= 1225) {
+		
+		//collect all inline vocab words (found across post text)
+		// var inlineVocabWordList = document.getElementsByClassName('vocabwordinline');
 
 		// looping through to remove the href attribute ...
 		// ...only above 1225px screenwidth, so that vocabword still anchor links to glossary at smaller screensizes
@@ -41,24 +42,28 @@ function vocabTest(e) {
 			// collect all definitions (found in glossary area)
 			var definitionsList = document.getElementsByClassName('vocabwordanddefinitionclass');
 
+			// where the definition will go
+			// var container = selectedVocab.parentNode.nextElementSibling;
+			// var container = selectedVocab.nextElementSibling;
+
 			// loop through this for loop code as many times as there are definitions 	
 	    	for (var i = 0; i < definitionsList.length; i++) {
 
 	    		// if selected inline vocab word matches one of the definitions being looped through...
 	    		if ((selectedVocab.getAttribute('id')) == ('-' + definitionsList[i].getAttribute('data-definition-id'))) {
 
+
 	    			var containerCheck = selectedVocab.nextElementSibling;
 
-	    			// it is already open
 	    			if (containerCheck) {
-	    				// close it (remove container)
 	    				containerCheck.parentNode.removeChild(containerCheck);
 	    			} else {
 
-    					var otherContainer = document.getElementById('definitioncontainer');
+	    			// close out any other open definitions
+    				var otherContainer = document.getElementById('definitioncontainer');
 
-    					// close out any other open definitions
 	    				if (otherContainer) {
+    						// otherContainer.parentNode.removeChild(otherContainer);
     						otherContainer.parentNode.removeChild(otherContainer);
     					}
 
@@ -67,12 +72,21 @@ function vocabTest(e) {
 		    			selectedVocab.parentNode.appendChild(container);
 
 		    			// give it attributes
+	    				// container.setAttribute('class', 'inlinevocabcontainer definitiontoggleoff');
 	    				container.setAttribute('class', 'inlinevocabcontainer');
 	    				container.setAttribute('id', 'definitioncontainer');
 
 	    				// populate the container with the definition HTML
 	    				// container.innerHTML = definitionsList[i].innerHTML;
-    					container.innerHTML = '<p id="vocabclose" data-vocab-x>CLOSE</p>' + definitionsList[i].innerHTML;    // temporary "X"
+    					container.innerHTML = '<p id="vocabclose">CLOSE</p>' + definitionsList[i].innerHTML;    // temporary "X"
+
+    					// toggle visibility
+    					// !!! FIX !!! Need a better way to toggle visibility...
+    					// ... this won't work with the container creation method I set up just above this
+    					// container.classList.toggle('definitiontoggleoff');
+
+	    				// track visibility with existence of data-vocab-visible attribute
+    					// selectedVocab.setAttribute('data-vocab-visible', '');
     				}
     			} 
     		} // close for loop
@@ -83,21 +97,14 @@ function vocabTest(e) {
 window.addEventListener('click', vocabTest, false);
 
 
-
 // !!! vocabX not being recognized because it doesn't exist until a vocabword is clicked
 // var vocabX = document.getElementById('vocabclose');
 
-function vocabXClose(e) {
-	
-	var clickedX = e.target;
-
-	if (clickedX.hasAttribute('data-vocab-x')) {
-		
-		var containerOfX = clickedX.parentNode;
-
-		containerOfX.parentNode.removeChild(containerOfX);
-
+function vocabXClose() {
+	if (vocabX) {
+		// vocabX.parentNode.setAttribute('class', 'definitiontoggleoff');
+		vocabX.innerHTML = 'TEST :D';
 	}
 }
 
-window.addEventListener('click', vocabXClose, false);
+//vocabX.addEventListener('click', vocabXClose, false);
