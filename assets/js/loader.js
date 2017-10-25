9599453b3fea35f8a0a76081084cef511881cb9e
Lia Prins <liajprins@gmail.com>
2017-10-24 21:59:27 -0500

    Safari rem bug fix in-progress
    Safari is not recognizing rems consistently, both inside and outside of
    media queries, and on text and non-text elements.

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -8,7 +8,6 @@ document.onreadystatechange = function () {
 }
 
 
-
 /* USE THIS ONE vvv FOR TESTING; IT HAS BUILT-IN TIME SLOT TO SHOW THE LOADER UPON REFRESH */
 /*
 document.onreadystatechange = function () {
@@ -20,7 +19,7 @@ document.onreadystatechange = function () {
          document.getElementById('interactive');
          document.getElementById('loadbg').style.visibility="hidden";
          document.getElementById('contents').style.visibility="visible";
-      },5000);
+      },10000);
   }
 }
 */
---
+++ site/templates/default-PRE-Safari-fix.php
@@ -0,0 +1,56 @@
+<?php snippet('head-open-sitewide') ?>
+
+
+<?php snippet('share-settings-common') ?>
+
+
+<?php snippet('share-settings-other') ?>
+
+
+<?php snippet('head-close') ?>
+
+
+    <?php snippet('menu') ?>
+
+    <!-- calling the JS scripts used on everypage -->
+    <?php snippet('scripts-sitewide') ?>
+
+
+    <!-- This encompasses everything within <body> except for <nav>
+    This is so that there is an element that can be clicked on that will be anything except <nav>,
+    that will be recognized in JS menu.js script, that can have an event listener applied when it is clicked on, that will close the <nav> -->
+    <div id="everythingexceptnav">
+
+
+    <main>
+
+        <div class="desktopcontent">
+
+            <!-- NOT SURE THAT THIS WILL STAY AS <h2> -->
+            <h2 class="extracontentpagetitle">
+                <?php echo $page->title()->html() ?>
+            </h2>
+
+            <!-- text for the page -->
+            <span class="l-textface">
+                <?php echo $page->text()->kirbytext() ?>
+            </span>
+    
+
+            <!-- Image of broken glasses -->
+
+            <!-- <img src= "<?php echo url('assets/images/broken-glasses.gif') ?>" alt="Illustration of broken glasses" id="brokenglasses"> -->
+            <img src= "<?php echo url('content/error/broken-glasses.jpg') ?>" alt="Illustration of broken glasses" id="brokenglasses">
+
+            <!--
+            <?php if(!$page->illustration()->empty()): ?>
+                <?php echo kirbytext($page->illustration()) ?>
+            <?php endif ?>
+            -->
+
+        </div>
+
+    </main>
+
+
+    <?php snippet('footer') ?>
