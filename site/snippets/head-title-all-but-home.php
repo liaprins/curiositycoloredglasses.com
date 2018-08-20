e2223e286d8fe291b961eccf4952802ff7730d99
Lia Prins <liajprins@gmail.com>
2018-08-19 20:00:40 -0500

    Remove messed up directory that had a merge of the code between CCG and portfolio by accident

--- site/snippets/head-title-all-but-home.php
+++
@@ -1,5 +0,0 @@
-  <!-- This is what shows when you bookmark this page, and what's in the browser tab -->
-  <title>
-    <?php echo $page->title()->html() ?> | Curiosity-Colored Glasses
-  </title>
-
