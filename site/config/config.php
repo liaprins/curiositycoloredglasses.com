6ae9f7fd5e5ca6d8dedb639739fb663fad341d83
Lia Prins <liajprins@gmail.com>
2018-08-27 09:27:20 -0500

    Added a purchased Kirby license to site/config/config.php

--- site/config/config.php
+++ site/config/config.php
@@ -15,7 +15,7 @@ for more information: http://getkirby.com/license
 
 */
 
-c::set('license', 'put your license key here');
+c::set('license', 'K2-PERSONAL-cf30bef4d286ec39af8316dc16eefac7');
 
 /*
 
