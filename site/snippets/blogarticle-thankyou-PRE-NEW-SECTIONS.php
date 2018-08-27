8ff98ec3610947fb6d2536f567456bfdc289a1e0
Lia Prins <liajprins@gmail.com>
2018-08-26 22:45:55 -0500

    Thank you and The method to my madness sections added

--- site/snippets/blogarticle-thankyou-PRE-NEW-SECTIONS.php
+++ site/snippets/blogarticle-thankyou-PRE-NEW-SECTIONS.php
@@ -1,7 +1,7 @@
 <!-- Trying three different ways to surface Thank You image + metadata -->
 <!-- Option 2: via markdown in each blogarticle.txt  -->
 <!-- This works for image only, no metadata (field needs to be put back into markdown) -->
-<?php if(!$page->thankyou()->empty()): ?>
+<?php if(!$page->thankyou2()->empty()): ?>
     
     <details class="postsectiondetails" open>
 
@@ -10,9 +10,7 @@
    		</summary>
 
     <!-- Calling the ThankYou field from the blogarticle.txt  -->
-    <span class="s-textface">
-    	<?php echo kirbytext($page->thankyou()) ?>
-    </span>
+    <?php echo kirbytext($page->thankyou()) ?>
 
 	</details>
 
